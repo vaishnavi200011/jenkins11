cb47ef507661aacf961eeca8c4e89dc1e34905a5
vaishnavi <sablevaishnavi07@gmail.com>
2022-11-11 11:15:22 +0530

    hello jenkins

---
+++ jenkins11/src/main/java/com/example/Jenkins11Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkins11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkins11Application.class, args);
+	}
+
+}
